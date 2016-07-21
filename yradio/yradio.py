3346c78e2de25fbbd26108c6c3d5e6395df4e520
Josh Tiras <tiras.j@gmail.com>
2016-07-21 15:18:32 -0700

    Render index.html

--- yradio/yradio.py
+++ yradio/yradio.py
@@ -71,7 +71,7 @@ def show_entries():
     db = get_db()
     cur = db.execute('select title, text from entries order by id desc')
     entries = cur.fetchall()
-    return render_template('show_entries.html', entries=entries)
+    return render_template('index.html', entries=entries)
 
 
 @app.route('/add', methods=['POST'])
