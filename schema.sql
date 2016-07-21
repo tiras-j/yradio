6da1d6649174b8723f3d99d812ec70d473fb211e
felfilali <fzelfilali@gmail.com>
2016-07-21 13:53:07 -0700

    schema is fine, still need to debug module err

--- schema.sql
+++ schema.sql
@@ -1,36 +1,37 @@
 -- user tables
 drop table if exists Users;
-create table Users(
-	USER_ID INT AUTO_INCREMENT NOT NULL,
+create table Users (
+        USER_ID INT AUTO_INCREMENT NOT NULL,
         USER_NAME TEXT NOT NULL,
         PASSWORD TEXT NOT NULL, -- this needs to be taken care off later for n
-	COMMENT TEXT, -- this needs to be taken care off later
+        COMMENT TEXT, -- this needs to be taken care off later
         DATE_JOINED TEXT DEFAULT CURRENT_TIMESTAMP, -- this needs to be taken care off later
-        PRIMARY KEY(USER_ID),
+        PRIMARY KEY(USER_ID)
 );
 
 
 -- playlist tables
 drop table if exists Playlists;
-create table Playlists(
+create table Playlists (
         PLAYLIST_ID INT AUTO_INCREMENT NOT NULL,
         PLAYLIST_NAME TEXT NOT NULL,
+        USER_ID TEXT NOT NULL,
         Tags TEXT NOT NULL, -- this will be limited by the size limit of mysql text fields
         DATE_ADDED TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
         COMMENT TEXT, -- is case users wanna add a comment
         LINK TEXT NOT NULL, -- this will be limited by the size limit of mysql text fields
         PRIMARY KEY(PLAYLIST_ID),
-        FOREIGN KEY(USER_ID) REFERENCES Users(USER_ID), -- who posted the playlist
+        FOREIGN KEY(USER_ID) REFERENCES Users(USER_ID) -- who posted the playlist
 );
 
 -- playlist tags this is for searching purposes,
 -- we might need some data cleanup to get rid off 
 -- tags like #music #awesome #pokemongo (this is for later)
 drop table if exists Tags;
-create table Tags(
+create table Tags (
         TAG_ID INT AUTO_INCREMENT NOT NULL, --just id
         TAG_NAME TEXT NOT NULL,
         PLAYLISTS_LIST TEXT NOT NULL, -- this will be limited by the size limit of mysql text fields
         DATE_ADDED TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        PRIMARY KEY(TAG_ID),
+        PRIMARY KEY(TAG_ID)
 );
---
+++ search.py
@@ -0,0 +1,28 @@
+# all the imports
+import os
+import sqlite3
+# import mysql
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'yradio.db'),
+    SECRET_KEY='development key',
+    USERNAME='root',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
